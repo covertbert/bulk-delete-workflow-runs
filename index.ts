5f48086becc6917ba75ac0057b70fe48bd76ac5c
Bertie Blackman <blackmanrgh@gmail.com>
2020-10-15 16:33:02 +0100

    Initial commit

--- index.ts
+++ index.ts
@@ -3,29 +3,38 @@ const [owner, repo, workflowName, githubPAT] = process.argv.slice(2)
 
 const octokit = new Octokit({ auth: githubPAT })
 
-const init = async () => {
-  const allWorkflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
-    owner,
-    repo,
-    per_page: 100,
-  })
+const getIDFromWorkflowName = async (workflowName: string) => {
+  try {
+    const allWorkflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
+      owner,
+      repo,
+      per_page: 100,
+    })
 
-  const idFromWorkflowName = allWorkflows.data.workflows.filter(
-    (workflow: { [key: string]: string }) => workflow.name === workflowName,
-  )[0]?.id
+    const idFromWorkflowName = allWorkflows.data.workflows.filter(
+      (workflow: { [key: string]: string }) => workflow.name === workflowName,
+    )[0]?.id
 
-  if (!idFromWorkflowName) {
-    throw new Error('No workflow exists with that name')
+    if (!idFromWorkflowName) {
+      throw new Error('No workflow exists with that name')
+    }
+
+    console.log(`Workflow ID is ${idFromWorkflowName}`)
+    return idFromWorkflowName
+  } catch (e) {
+    console.log('Failed to get all workflows')
+
+    throw new Error(e)
   }
+}
 
-  console.log(`Workflow ID is ${idFromWorkflowName}`)
-
+const init = async () => {
   const workflowRunsByID = await octokit.request(
     'GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs',
     {
       owner,
       repo,
-      workflow_id: idFromWorkflowName,
+      workflow_id: await getIDFromWorkflowName(workflowName),
       per_page: 100,
     },
   )
